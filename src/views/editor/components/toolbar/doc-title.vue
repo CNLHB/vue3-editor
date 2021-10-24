6282fe7c9008aa99faf7814ab1f7184bad04fc09
aiwa <1348844909@qq.com>
2021-10-24 22:33:19 +0800

    feat(/editor):增加文档标题

---
+++ src/views/editor/components/toolbar/doc-title.vue
@@ -0,0 +1,46 @@
+<template>
+        <el-input
+        v-if="inputVisible"
+        ref="saveTagInput"
+        v-model="inputValue"
+        class="input-new-tag"
+        size="mini"
+        @keyup.enter="handleInputConfirm"
+        @blur="handleInputConfirm"
+      >
+      </el-input>
+      <el-button v-else class="button-new-tag" size="small" @click="showInput"
+        >文档标题</el-button
+      >
+</template>
+
+<script>
+import {reactive,toRefs} from 'vue'
+export default {
+  name: "tool-right",
+  setup(props) {
+    const state = reactive({
+        inputVisible:false,
+        inputValue:'文档标题'
+    })
+    function handleInputConfirm(){
+        state.inputVisible = false
+
+    }
+    function showInput(){
+        state.inputVisible = true
+    }
+    return {
+        ...toRefs(state),
+        handleInputConfirm,
+        showInput
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.t_right{
+  justify-self: flex-end;
+}
+</style>
--- src/views/editor/components/toolbar/tool-left.vue
+++ src/views/editor/components/toolbar/tool-left.vue
@@ -1,124 +1,118 @@
 <template>
   <div class="t_left">
-      <div class="logo">
-    <a href="/editor/index">
-      海报设计
-    </a>
-  </div>
-  <div class="l_setting">
-    <div>
-      <el-button class="custom_btn" type="text">
-        设置
-      </el-button>
+    <div class="logo">
+      <a href="/editor/index"> 海报设计 </a>
     </div>
-    <div>
-      <el-button class="custom_btn" type="text">
-        视图
-      </el-button>
-      
+    <div class="l_setting">
+      <div>
+        <el-button class="custom_btn" type="text"> 设置 </el-button>
+      </div>
+      <div>
+        <el-button class="custom_btn" type="text"> 视图 </el-button>
+      </div>
     </div>
-  </div>
-     <div class="tool_line"></div>
+    <div class="tool_line"></div>
 
-    <div class="l_action ">
-      <el-button  class="custom_btn disabled" type="text">
+    <div class="l_action">
+      <el-button class="custom_btn disabled" type="text">
         <span class="action_btn undo"></span>
       </el-button>
-     <el-button class="custom_btn custom_btn_redo" type="text">
+      <el-button class="custom_btn custom_btn_redo" type="text">
         <span class="action_btn redo disabled"></span>
       </el-button>
     </div>
-    <div>
-
-  </div>
+    <div class="doc_title"> 
+        <DocTitle></DocTitle>
+    </div>
   </div>
 </template>
 
 <script>
-import {reactive,toRefs} from 'vue'
+import { reactive, toRefs } from 'vue'
 import { useMouse, usePreferredDark, useLocalStorage } from '@vueuse/core'
-
-
+import DocTitle from './doc-title.vue'
 export default {
-  name: "tool-left",
+  name: 'tool-left',
+  components:{
+    DocTitle
+  },
   setup(props) {
     const state = reactive({
-      domRef:null
+      domRef: null,
     })
     // tracks mouse position
-    const { x, y } = useMouse({
-    })
+    const { x, y } = useMouse({})
 
-
-    return { x, y}
+    return { x, y }
     return {
-        ...toRefs(state)
+      ...toRefs(state),
     }
-  }
+  },
 }
 </script>
 
 <style scoped lang="less">
-.t_left{
+.t_left {
   display: flex;
   align-items: center;
 }
-.logo{
+.logo {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-right: 40px;
-
 }
-.l_setting{
+.l_setting {
   display: flex;
   align-items: center;
 }
 
-  .custom_btn{
-    padding: 0 12px;
-    color: #444950;
-    &:hover{
-      background-color: #f1f2f4;
-      border-color: #f1f2f4;
-    }
+.custom_btn {
+  padding: 0 12px;
+  color: #444950;
+  &:hover {
+    background-color: #f1f2f4;
+    border-color: #f1f2f4;
+  }
 
-    &.custom_btn_redo{
-      margin-left:0;
+  &.custom_btn_redo {
+    margin-left: 0;
+  }
+}
+.tool_line {
+  width: 0;
+  height: 20px;
+  margin: 0 8px;
+  border-left: 1px solid #dadde1;
+}
+.l_action {
+  display: flex;
+  .action_btn {
+    display: inline-block;
+    font-size: 12px;
+    padding: 0;
+    width: 20px;
+    height: 20px;
+  }
+  .undo {
+    background: url(../../imgs/redo_ennable.png) no-repeat center;
+    transform: rotateY(180deg);
+    &.disabled {
+      background: url(../../imgs/undo_disable.png) no-repeat center;
+      transform: rotateY(0deg);
+      cursor: not-allowed;
     }
   }
-.tool_line{
-    width: 0;
-    height: 20px;
-    margin: 0 8px;
-    border-left: 1px solid #dadde1;
-}
-.l_action{
-  display: flex;
-    .action_btn{
-      display: inline-block;
-      font-size: 12px;
-      padding: 0;
-      width: 20px;
-      height: 20px;
-    }
-    .undo{
-      background: url(../../imgs/redo_ennable.png) no-repeat center;
+  .redo {
+    background: url(../../imgs/redo_ennable.png) no-repeat center;
+    &.disabled {
+      background: url(../../imgs/undo_disable.png) no-repeat center;
       transform: rotateY(180deg);
-      &.disabled{
-        background: url(../../imgs/undo_disable.png) no-repeat center;
-        transform: rotateY(0deg);
-        cursor: not-allowed;
-
-      }
+      cursor: not-allowed;
     }
-    .redo{
-      background: url(../../imgs/redo_ennable.png) no-repeat center;
-      &.disabled{
-        background: url(../../imgs/undo_disable.png) no-repeat center;
-        transform: rotateY(180deg);
-        cursor: not-allowed;
-      }
-    }
+  }
+}
+.doc_title{
+  margin-left: 20px;
 }
 </style>
